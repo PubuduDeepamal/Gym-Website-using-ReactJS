177ccd4ecfcf2adf664a6bb423a664af5f3d7452
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2024-05-05 08:54:43 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, {usestate} from "react";
+import Navbar from "./Components/Navbar";
+import logo from "../src/images/logo.png";
+// import {Link} from "react-scrool";
 
 function App() {
   return (
     <div className="App">
+
+      <Navbar/>
     
     </div>
   );
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+function Navbar() {
+
+  const [nav, setnav] = useState(false);
+
+  const changeBackground = () =>{
+    
+  }
+
+  return (
+    <nav>
+
+    </nav>
+  )
+}
+
+export default Navbar
